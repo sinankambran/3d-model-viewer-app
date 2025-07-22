2d77bfdd713a5549ede300809748f27cf9825db1
Mohammed Sinan <sinanksinu0@gmail.com>
2025-07-21 23:00:43 -0700

    first commit 2

--- backend/routes/modelRoutes.js
+++ backend/routes/modelRoutes.js
@@ -4,30 +4,52 @@ const fs = require("fs");
 const upload = require("../middleware/upload");
 const Model = require("../models/Model");
 const cloudinary = require("../utils/cloudinary");
+const streamifier = require("streamifier");
+
+
 
 // POST upload - Upload 3D model file
 router.post("/upload", upload.single("model"), async (req, res) => {
   try {
-    const result = await cloudinary.uploader.upload(req.file.path, {
-      resource_type: "raw",
-      folder: "models"
-    });
 
-    const model = new Model({
+  if(!req.file){
+   return res.status(400).json({error: "No file uploaded"})
+  }
+   const streamUpload = (buffer) => {
+    return new Promise((resolve , rejects) => {
+      const stream = cloudinary.uploader.upload_stream(
+        {
+          resource_type: "raw",
+          folder: "models",
+        },
+        (error , result) => {
+          if(result) resolve(result);
+          else rejects(error);
+
+          
+        }
+      );
+      streamifier.createReadStream(buffer).pipe(stream);
+    });
+   };
+   const result = await streamUpload(req.file.buffer);
+
+       const model = new Model({
       filename: req.file.originalname,
       url: result.secure_url,
       public_id: result.public_id,
-      contentType: req.file.mimetype
+      contentType: req.file.mimetype,
     });
 
-    await model.save();
-    fs.unlinkSync(req.file.path);
+    await model.save()
 
     res.status(201).json(model);
-  } catch (err) {
-    console.error("Upload error:", err);
-    res.status(500).json({ error: err.message });
-  }
+
+} catch (err) {
+  console.error("Upload error:" , err);
+  res.status(500).json({error: err.message})
+  
+}
 });
 
 // GET models - List all models
