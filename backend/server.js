b6a1567356b94449bb677978bf1c7aec66ea8e2c
Mohammed Sinan <sinanksinu0@gmail.com>
2025-07-21 23:47:17 -0700

    first commit 7

--- backend/server.js
+++ backend/server.js
@@ -27,10 +27,7 @@ app.use(express.json());
 app.use('/model', express.static(path.join(__dirname, 'uploads')));
 app.use('/', modelRoutes);
 
-mongoose.connect(process.env.MONGO_URI ,{
-   useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log('MongoDB connected');
     const PORT = process.env.PORT || 5000;
